209e16af67e47bd18465c0ef283b182c62c55481
Kierra M <k.merriman32@hotmail.com>
2019-11-14 03:00:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,5 @@ class App < Sinatra::Base
 	get '/' do
 		erb:index 
 	end
-	#This tells Sinatra to render a file called index.erb inside of a directory called views.
+	#This tells Sinatra to render a file called index.erb inside of a directory called views. sintra autmatically looks for views folder
 end
