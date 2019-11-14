f345282c35d80404f6c7e3f56bb90a1b18e727d2
Kierra M <k.merriman32@hotmail.com>
2019-11-14 02:59:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		erb:index #This tells Sinatra to render a file called index.erb inside of a directory called views.
+		erb:index 
 	end
+	#This tells Sinatra to render a file called index.erb inside of a directory called views.
 end
